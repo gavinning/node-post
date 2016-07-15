e267470761175f6853b720a09b99d49a5e9e7e57
gavinning <ningyubo@gmail.com>
2016-07-15 18:02:19 +0800

    v0.1.1 1.Fixbug 修正aimee-is没有正确添加依赖的问题 2.Fixbug 修正初始化不能正确创建配置文件的问题 3.Fixbug 修正因post.config.posterSelectedIndex值为空引起的问题

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var is = require('aimee-is');
 var path = require('path');
 var post = require('./lib/post');
+var filepath = path.join(process.env.HOME, '.postconfig.json');
 
 // 初始化配置文件路径
-post.config.init(path.join(process.env.HOME, '.postconfig.json'));
+post.create(filepath);
+post.config.init(filepath);
 
 if(is.emptyObject(post.config.get())){
     post.config.merge({
@@ -16,7 +18,8 @@ if(is.emptyObject(post.config.get())){
                 name: 'prod',
                 url: 'http://prod:prod@127.0.0.1/upload'
             }
-        ]
+        ],
+        posterSelectedIndex: 0
     });
     post.config.save({pretty: true});
 }
